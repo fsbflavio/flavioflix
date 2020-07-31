9849d55086993cb231b5f0f0c58d83f22b77ee4b
flavio <flavio.baia@pronetsoftware.com.br>
2020-07-31 18:02:44 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import Menu from './componets/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './componets/BannerMain';
+import Carousel from './componets/Carousel';
+
 
 function App() {
   return (
-    <div>
-      <Menu/>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"descrição video"}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFistVideo
+        category={dadosIniciais.categorias[5]}
+      />
     </div>
   );
 }
