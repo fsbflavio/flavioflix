bc6188ec2c79a093deb0e21f5be5c2d7fd275f09
Flavio <flavio.baia@pronetsoftware.com.br>
2020-08-05 23:32:10 -0300

    add cad video

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-
+import categoriasRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div style={{ background: "#141414" }}>
+    <PageDefault>
       <Menu />
 
-      <BannerMain
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
+
+      {dadosIniciais.map((item, index) => {
+        if (index === 0) {
+          return (
+            <div key={item.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="descrição video"
+              />
+
+              <Carousel
+                ignoreFistVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
+        return (
+          <Carousel
+            key={item.id}
+            category={dadosIniciais[index]}
+          />
+        );
+      })}
+
+      {/*<BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"descrição video"}
+        videoDescription="descrição video"
       />
 
       <Carousel
@@ -44,8 +83,9 @@ function Home() {
       <Carousel
         ignoreFistVideo
         category={dadosIniciais.categorias[5]}
-      />
-    </div>
+      />*/}
+
+    </PageDefault>
   );
 }
 
